a8729824b0d4ebde17344ea4b8baf551b35c9f4c
Harishs0524 <harishs0524@gmail.com>
2026-02-28 12:31:26 -0600

    UC3 - Implement String Join feature

--- Harishbannerapp1.java
+++ Harishbannerapp1.java
@@ -1,11 +1,11 @@
 public class Harishbannerapp1 {
     public static void main(String[] args) { 
-        System.out.println("  ***    ***    *****   ***** ");
-        System.out.println(" *   *  *   *   *    *  *     ");
-        System.out.println("*     **     *  *    *  *     ");
-        System.out.println("*     **     *  *****   ***** ");
-        System.out.println("*     **     *  *           * ");
-        System.out.println(" *   *  *   *   *           * ");
-        System.out.println("  ***    ***    *       ***** ");       
+        System.out.println(String.join("","  ***    ***    *****   ***** "));
+        System.out.println(String.join(""," *   *  *   *   *    *  *     "));
+        System.out.println(String.join("","*     **     *  *    *  *     "));
+        System.out.println(String.join("","*     **     *  *****   ***** "));
+        System.out.println(String.join("","*     **     *  *           * "));
+        System.out.println(String.join(""," *   *  *   *   *           * "));
+        System.out.println(String.join("","  ***    ***    *       ***** "));
     }
 }
