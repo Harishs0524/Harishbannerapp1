8d0144369dfdcc721af35da8c3d77df4d1ccd810
Harishs0524 <harishs0524@gmail.com>
2026-02-28 11:43:00 -0600

    UC2 - Print OOPS Banner

--- Harishbannerapp1.java
+++ Harishbannerapp1.java
@@ -1,7 +1,11 @@
 public class Harishbannerapp1 {
-    public static void main(String[] args) {
-         
-        System.out.println("OOPS");
-
+    public static void main(String[] args) { 
+        System.out.println("  ***    ***    *****   ***** ");
+        System.out.println(" *   *  *   *   *    *  *     ");
+        System.out.println("*     **     *  *    *  *     ");
+        System.out.println("*     **     *  *****   ***** ");
+        System.out.println("*     **     *  *           * ");
+        System.out.println(" *   *  *   *   *           * ");
+        System.out.println("  ***    ***    *       ***** ");       
     }
 }
