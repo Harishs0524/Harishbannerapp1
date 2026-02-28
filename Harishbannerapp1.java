04bab64b9604f186bfae7fcb6faddcd1bc987cdf
Harishs0524 <harishs0524@gmail.com>
2026-02-28 12:49:21 -0600

    UC4 - Implement Array Loop banner

--- Harishbannerapp1.java
+++ Harishbannerapp1.java
@@ -1,11 +1,19 @@
 public class Harishbannerapp1 {
-    public static void main(String[] args) { 
-        System.out.println(String.join("","  ***    ***    *****   ***** "));
-        System.out.println(String.join(""," *   *  *   *   *    *  *     "));
-        System.out.println(String.join("","*     **     *  *    *  *     "));
-        System.out.println(String.join("","*     **     *  *****   ***** "));
-        System.out.println(String.join("","*     **     *  *           * "));
-        System.out.println(String.join(""," *   *  *   *   *           * "));
-        System.out.println(String.join("","  ***    ***    *       ***** "));
+    public static void main(String[] args) {
+
+        String[] lines = new String[7];
+
+        lines[0] = "  ***    ***    *****   ***** ";
+        lines[1] = " *   *  *   *   *    *  *     ";
+        lines[2] = "*     **     *  *    *  *     ";
+        lines[3] = "*     **     *  *****   ***** ";
+        lines[4] = "*     **     *  *           * ";
+        lines[5] = " *   *  *   *   *           * ";
+        lines[6] = "  ***    ***    *       ***** ";
+
+        for (String line:lines) {
+            System.out.println(line);
+        }
+        
     }
 }
